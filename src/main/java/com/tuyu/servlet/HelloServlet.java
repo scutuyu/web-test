f4991dfdee9e4c3b1531551534c928a68162c890
tuyu <scutuyu@foxmail.com>
2018-01-18 09:45:26 +0800

    init

---
+++ src/main/java/com/tuyu/servlet/HelloServlet.java
@@ -0,0 +1,37 @@
+package com.tuyu.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author tuyu
+ */
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("hello tuyu.");
+        writer.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        System.out.println("hello servlet 销毁");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        System.out.println("hello servlet 初始化");
+    }
+}
